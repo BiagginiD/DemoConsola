98cf5b49ed7d15372af8cc5272f46e8b8538ca7a
Biaggini Durán Ribera <biaggini.duran@inetum.world>
2021-10-21 17:18:37 +0200

    DemoConsola

---
+++ src/main/java/com/gabrielCode/repository/IPersona.java
@@ -0,0 +1,7 @@
+package com.gabrielCode.repository;
+
+public interface IPersona {
+
+	void registrar(String pNombre);
+	
+}
---
+++ src/main/java/com/gabrielCode/service/IpersonaService.java
@@ -0,0 +1,7 @@
+package com.gabrielCode.service;
+
+public interface IpersonaService {
+	
+	void registrarHandler(String pNombre);
+
+}
